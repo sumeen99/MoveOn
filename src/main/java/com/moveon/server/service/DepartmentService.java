48db642c8eef38e4a1680746d89f4ad13c07fe12
sumeen99 <70104259+sumeen99@users.noreply.github.com>
2021-11-17 06:17:54 +0900

    querydsl 적용중... 어렵다............

---
+++ src/main/java/com/moveon/server/service/DepartmentService.java
@@ -0,0 +1,15 @@
+package com.moveon.server.service;
+
+import com.moveon.server.repository.Department.DepartmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DepartmentService {
+    private final DepartmentRepository departmentRepository;
+
+
+
+
+}
